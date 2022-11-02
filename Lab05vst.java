9d6c856c53a21f334c7ac3f19ada73d2c719dd11
Gabe <gabriel.lupino@vtsd.com>
2022-11-02 08:41:53 -0400

    I did it

--- Lab05vst.java
+++ Lab05vst.java
@@ -12,11 +12,44 @@ public class Lab05vst extends Applet
     {
         int width = 980;
         int height = 630;
-        int x1 = 10;
+        int x1;
         int y1 = 640;
         int x2 = 990;
-        int y2 = 640;
+        int y2 = 630;
         g.drawRect(10,10,width,height);
+
+        //draw bottom right
+        for(x1 = 10; x1 < 990; x1+=14){
+            g.drawLine(x1,y1,x2,y2);
+            y2 -= 9;
+        }
+
+        //draw bottom left
+        x2 = 10;
+        y1 = 640;
+        y2 = 630;
+        for(x1 = 990; x1 > 10; x1-=14){
+            g.drawLine(x1, y1, x2, y2);
+            y2 -= 9;
+        }
+        //Draw top left
+        x2 = 10;
+        y1 = 10;
+        y2 = 10;
+        for(x1 = 990; x1 > 10; x1-=14){
+            g.drawLine(x1, y1, x2, y2);
+            y2 += 9;
+        }
+        //Draw top right
+        x2 = 990;
+        y1 = 10;
+        y2 = 10;
+        for(x1 = 10; x1 < 990; x1+=14){
+            g.drawLine(x1, y1, x2, y2);
+            y2 += 9;
+        }
+
+        }
     }
 }
 
